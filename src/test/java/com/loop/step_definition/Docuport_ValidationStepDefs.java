fa6a5f6f0cbb988021c18edf71cdd5f864164e22
sergii <davygorasergii@gmail.com>
2024-02-06 14:53:55 -0800

    new commit4546523

--- src/test/java/com/loop/step_definition/Docuport_ValidationStepDefs.java
+++ src/test/java/com/loop/step_definition/Docuport_ValidationStepDefs.java
@@ -12,21 +12,25 @@ public class Docuport_ValidationStepDefs {
     public void login_as(String User) {
         docuportLogin.login(User);
     }
-    @When("click {string}")
-    public void click(String receiveDocs) {
-        docuportLogin.clickOn(receiveDocs);
+
+    @When("click on {string}")
+    public void click_on(String myuploads) {
+        docuportLogin.clickon(myuploads);
+
     }
+
     @Then("Validate Received docs ,search button, download button  is displayed")
     public void validate_received_docs_search_button_download_button_is_displayed() {
         docuportLogin.receivDocs.isDisplayed();
         docuportLogin.search.isDisplayed();
         docuportLogin.download.isDisplayed();
     }
-    @When("click on {string}")
-    public void click_on(String myuploads) {
-        docuportLogin.clickon(myuploads);
 
+    @When("click {string}")
+    public void click(String receiveDocs) {
+        docuportLogin.clickOn(receiveDocs);
     }
+
     @Then("Validate My uploads, download button, search button is displayed")
     public void validate_my_uploads_download_button_search_button_is_displayed() {
         docuportLogin.myUpload.isDisplayed();
