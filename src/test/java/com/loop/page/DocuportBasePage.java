6b7a8d29b8c05a01c3d66160e4916a63e9c9ee44
sergii <davygorasergii@gmail.com>
2024-02-06 17:14:16 -0800

    new commit4546523211

--- src/test/java/com/loop/page/DocuportBasePage.java
+++ src/test/java/com/loop/page/DocuportBasePage.java
@@ -53,7 +53,7 @@ public class DocuportBasePage {
     public WebElement receivedDocsButton;
     @FindBy(xpath ="//h1[@class='text-h6 font-weight-bold']" )
     public WebElement receivDocs;
-    @FindBy(xpath = "//span[text()='My uploads'] ")
+    @FindBy(xpath = "//div[@class='v-list-item__title']/span[text()='My uploads']")
     public WebElement myUploadsButton;
     @FindBy(xpath = "//h1[@class='text-h6 font-weight-bold']")
     public WebElement myUpload;
