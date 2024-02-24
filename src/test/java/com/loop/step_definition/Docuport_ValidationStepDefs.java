8e1baf2afe7a5329c68db6092de42fc001fd71d0
sergii <davygorasergii@gmail.com>
2024-02-24 07:00:18 -0800

    new push

--- src/test/java/com/loop/step_definition/Docuport_ValidationStepDefs.java
+++ src/test/java/com/loop/step_definition/Docuport_ValidationStepDefs.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.ElementNotInteractableException;
+import utilities.BrowserUtils;
 
 public class Docuport_ValidationStepDefs {
 
@@ -12,14 +13,20 @@ public class Docuport_ValidationStepDefs {
 
     @Given("login as {string}")
     public void login_as(String user) {
+        BrowserUtils.justWait(5000);
+
         docuportLogin.login(user);
     }
     @When("click on {string}")
     public void click_on(String userClick) {
+        BrowserUtils.justWait(5000);
+
         docuportLogin.clickButton(userClick);
     }
     @Then("Validate {string},download button, search button is displayed")
     public void validate_download_button_search_button_is_displayed(String item) {
+        BrowserUtils.justWait(5000);
+
         docuportLogin.displayed(item);
         docuportLogin.search.isDisplayed();
         docuportLogin.download.isDisplayed();
@@ -27,6 +34,8 @@ public class Docuport_ValidationStepDefs {
     }
     @Then("LogOut")
     public void log_out() {
+        BrowserUtils.justWait(5000);
+
         docuportLogin.logOut();
     }
 
